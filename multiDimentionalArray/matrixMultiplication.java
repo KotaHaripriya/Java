5f957c0f20169045f30e82a9562e3850a957f248
KotaHaripriya <kotaharipriya55@gmail.com>
2026-08-31 11:58:00 +0530

    updated

---
+++ multiDimentionalArray/matrixMultiplication.java
@@ -0,0 +1,11 @@
+package multiDimentionalArray;
+
+public class matrixMultiplication {
+    public static void main(String[] args) {
+        int[][] matrix1={{1,2},{3,4}};
+        int[][] matrix2={{1,2},{3,4}};
+        
+    }
+   
+
+}
